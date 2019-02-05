ba47db292ffa2982b61f05ca87a0647205569f54
Fred Sena <sena.fred@gmail.com>
2019-02-05 06:04:11 +0000

    Implemented tests for observer pattern

---
+++ DesignPatterns.Core/Observer/WeatherDataObserver/Observer/StatisticsDisplay.cs
@@ -0,0 +1,47 @@
+﻿using DesignPatterns.Core.Observer.WeatherDataObserver.Subject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Core.Observer.WeatherDataObserver.Observer
+{
+    public class StatisticsDisplay : IObserver, IDisplayElement
+    {
+        private float maxTemp = 0.0f;
+        private float minTemp = 200;
+        private float tempSum = 0.0f;
+        private int numReadings;
+        private WeatherData _weatherData;
+
+        public StatisticsDisplay(WeatherData weatherData)
+        {
+            _weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            tempSum += temp;
+            numReadings++;
+
+            if (temp > maxTemp)
+            {
+                maxTemp = temp;
+            }
+
+            if (temp < minTemp)
+            {
+                minTemp = temp;
+            }
+
+            Display();
+        }
+
+        public string Display()
+        {
+            return "Avg/Max/Min temperature = " + (tempSum / numReadings) + "/" + maxTemp + "/" + minTemp;
+        }
+
+
+    }
+}
---
+++ DesignPatterns.Tests/WeatherDataObserverTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using FluentAssertions;
+using DesignPatterns.Core.Observer.WeatherDataObserver.Subject;
+using DesignPatterns.Core.Observer.WeatherDataObserver.Observer;
+
+namespace DesignPatterns.Tests
+{
+    public class WeatherDataObserverTests
+    {
+        [Fact]
+        public void TestMethod1_WeatherStation()
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+            weatherData.SetMeasurements(78, 90, 29.2f);
+
+            var result = currentDisplay.Display();
+
+        }
+
+        [Fact]
+        public void Test_WeatherStationHeatIndex()
+        {
+            WeatherData weatherData = new WeatherData();
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+            weatherData.SetMeasurements(78, 90, 29.2f);
+
+            var result = statisticsDisplay.Display();
+        }
+    }
+}
